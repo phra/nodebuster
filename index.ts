aa76d2c7ae80fc07a5f5f1b8f26983726d5d7218
phra <greensoncio@gmail.com>
2018-04-12 12:28:41 +0200

    feat: improve ui

--- index.ts
+++ index.ts
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 import caporal = require('caporal')
 
+console.log = console.error.bind(console)
+import animation = require('chalk-animation')
+
 import { dir } from './lib'
 import { IOptions } from './models'
+
+// tslint:disable-next-line:no-var-requires
+const version = require('./package.json').version
+
+const BANNER = `[!] nodebuster v.${version}`
+
+const effects = ['rainbow', 'pulse', 'glitch', 'radar', 'neon', 'karaoke']
+
+const animator = animation[effects[(Math.random() * 100 % (effects.length - 1)).toFixed(0)]]
 
 caporal
   .version('1.0.0')
@@ -15,8 +27,17 @@ caporal
   .option('--extensions <extensions>', 'Extensions to use', caporal.LIST, [])
   .option('--cookies', 'Set <cookies>', caporal.LIST, [])
   .option('--ignore-ssl', 'Skip SSL/TLS certificate check', caporal.LIST, [])
+  .option('--version', 'Print version and exit', caporal.BOOLEAN, false)
   .action((args, options, logger) => {
-    dir(logger, args.url, options as IOptions)
+    if (options.version) {
+      return logger.info(BANNER)
+    }
+
+    const _ = animator(BANNER)
+    setTimeout(() => {
+      _.stop()
+      dir(logger, args.url, options as IOptions)
+    }, 2000)
   })
 
 caporal.parse(process.argv)
