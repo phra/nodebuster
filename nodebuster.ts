1847004f2a941393037e48febb57c0a047105642
phra <greensoncio@gmail.com>
2018-06-01 19:38:55 +0200

    feat: implement cli shorthands, fixes #12

--- nodebuster.ts
+++ nodebuster.ts
@@ -21,13 +21,13 @@ caporal
   .command('dir', 'Directory bruteforce mode')
   // tslint:disable-next-line:max-line-length
   .argument('<url>', '<url> to attack', /^https?:\/\//)
-  .option('--workers <workers>', 'Use n <workers>', caporal.INT, 10)
-  .option('--wordlist <wordlist>', 'Wordlist to use', caporal.STRING, '/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt')
-  .option('--user-agent <user-agent>', 'User-agent to use', caporal.STRING, 'nodebuster')
-  .option('--extensions <extensions>', 'Extensions to use', caporal.LIST, [])
-  .option('--cookies', 'Set <cookies>', caporal.LIST, [])
-  .option('--ignore-ssl', 'Skip SSL/TLS certificate check', caporal.BOOLEAN, false)
-  .option('--consecutive-fails', 'Stop after <consecutive-fails>', caporal.INT, 15)
+  .option('-w, --workers <workers>', 'Use n <workers>', caporal.INT, 10)
+  .option('-W, --wordlist <wordlist>', '<wordlist> to use', caporal.STRING, '/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt')
+  .option('-u, --user-agent <user-agent>', '<user-agent> to use', caporal.STRING, 'nodebuster')
+  .option('-e, --extensions <extensions>', '<extensions> to use', caporal.LIST, [])
+  .option('-c, --cookies <cookies>', 'Set <cookies>', caporal.LIST, [])
+  .option('-K, --ignore-ssl', 'Enable <ignore-ssl>', caporal.BOOLEAN, false)
+  .option('-f, --consecutive-fails', 'Stop after <consecutive-fails>', caporal.INT, 15)
   .action((args, options, logger) => {
     const _ = animator(BANNER)
     setTimeout(() => {
