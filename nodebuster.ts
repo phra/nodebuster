eec186daf749285a2b671809272bc31a9e9d9cab
phra <greensoncio@gmail.com>
2018-10-08 22:51:32 +0200

    default to wappalyzer if no extensions are specified
    temporary fix #27

--- nodebuster.ts
+++ nodebuster.ts
@@ -18,13 +18,11 @@ caporal
   .command('dir', 'Directory bruteforce mode')
   // tslint:disable-next-line:max-line-length
   .argument('<url>', '<url> to attack', /^https?:\/\//)
-  .option('-a, --auto', 'Use Wappalyzer to <auto> detect extensions', caporal.BOOLEAN, false)
-  .option('-n, --no-wappalyzer', 'Disable Wappalyzer completely', caporal.BOOLEAN, false)
   .option('-w, --workers <workers>', 'Use n <workers>', caporal.INT, 10)
   .option('-W, --wordlist <wordlist>', '<wordlist> to use', caporal.STRING, '/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt')
-  .option('-u, --user-agent <user-agent>', '<user-agent> to use', caporal.STRING, 'nodebuster')
+  .option('-U, --user-agent <user-agent>', '<user-agent> to use', caporal.STRING, 'nodebuster')
   .option('-e, --extensions <extensions>', '<extensions> to use', caporal.LIST, [])
-  .option('-c, --cookies <cookies>', 'Set <cookies>', caporal.REPEATABLE, [])
+  .option('-C, --cookies <cookies>', 'Set <cookies>', caporal.REPEATABLE, [])
   .option('-H, --headers <headers>', '<headers> to use', caporal.REPEATABLE, [])
   .option('-K, --ignore-ssl', 'Enable <ignore-ssl>', caporal.BOOLEAN, false)
   .option('-f, --consecutive-fails', 'Stop after <consecutive-fails>', caporal.INT, 15)
@@ -32,18 +30,18 @@ caporal
     const _ = animation.rainbow(BANNER)
     setTimeout(() => {
       _.stop()
-      if (!options.noWappalyzer) {
+      if (!options.extensions.length) {
         const __ = animation.rainbow(WAPPALAZYER_BANNER)
         wappalyzerScan(args.url).then((results) => {
           __.stop()
           printResults(results)
-          if (options.auto) {
-            options.extensions = selectExtensions(results)
-            process.stdout.write(`Using extensions: ${options.extensions.join()}\n`)
-          }
-
+          options.extensions = selectExtensions(results)
+          process.stdout.write(`[?] Using extensions: ${options.extensions.join()}\n`)
           dir(logger, args.url, options as IOptions)
-        })
+        }).catch((err) => console.error(err))
+      } else {
+        process.stdout.write(`[?] Using extensions: ${options.extensions.join()}\n`)
+        dir(logger, args.url, options as IOptions)
       }
     }, 2000)
   })
